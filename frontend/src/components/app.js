8f9f926e867c34be8414766f8fa7cc7b938979c3
MO99ME99 <m.m.hosseini1099@gmail.com>
2024-02-08 22:18:48 +0330

    add CreateRoomPage + HomeRoomPage + RoomJoinPage react components

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,14 +1,23 @@
 import React, {Component} from "react"
 import {render} from "react-dom"
-
+import HomePage from "./HomePage"
+import RoomJoinPage from "./RoomJoinPage"
+import CreateRoomPage from "./CreateRoomPage"
 export default class App extends Component{
     constructor(props){
         super(props)
     }
     render(){
-        return <h1> Testing React Code </h1>
+        return (
+            <div>
+                <HomePage />
+                <RoomJoinPage />
+                <CreateRoomPage />
+            </div>
+        );
+
     }
 }
 
 const appDiv = document.getElementById('app');
-render(<App/>, appDiv)
+render(<App />, appDiv)
